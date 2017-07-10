290bf7f29e978c8af5cb18be265da5336c7f4c7d
Wun Chiou <wun@utdallas.edu>
2017-07-10 12:53:58 -0500

    Shows user info on dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,57 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <?php $user = Auth::user(); ?>
+                    @if($user)
+                    <p>You are logged in! You are user #{{ $user->id }}. Here's your info:</p>
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Attribute</th>
+                                <th>Value</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>NetID</td>
+                                <td>{{ $user->name }}</td>
+                            </tr>
+                            <tr>
+                                <td>Display Name</td>
+                                <td>{{ $user->display_name }}</td>
+                            </tr>
+                            <tr>
+                                <td>First Name</td>
+                                <td>{{ $user->firstname }}</td>
+                            </tr>
+                            <tr>
+                                <td>Last Name</td>
+                                <td>{{ $user->lastname }}</td>
+                            </tr>
+                            <tr>
+                                <td>Email</td>
+                                <td>{{ $user->email }}</td>
+                            </tr>
+                            <tr>
+                                <td>PEA</td>
+                                <td>{{ $user->pea or 'none' }}</td>
+                            </tr>
+                            <tr>
+                                <td>Department</td>
+                                <td>{{ $user->department or 'none' }}</td>
+                            </tr>
+                            <tr>
+                                <td>Title</td>
+                                <td>{{ $user->title or 'none' }}</td>
+                            </tr>
+                            <tr>
+                                <td>College</td>
+                                <td>{{ $user->college or 'none' }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    @endif
                 </div>
             </div>
         </div>
